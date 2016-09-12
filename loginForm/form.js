1e1c625f9872cbae5e27a0b3c39588bee1007833
chen1218chen <cc.2008.com@163.com>
2016-09-12 16:26:28 +0800

    ''

---
+++ loginForm/form.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var myApp = angular.module('myApp',[]);
+myApp.controller('loginCtrl',['$scope', function($scope){
+
+	$scope.user={
+	/*	name: "ch",
+		password: 1,
+		autoLogin: true*/
+	};
+
+	$scope.submitForm = function(){
+		console.dir($scope.user);
+		$.ajax({
+			type:"post",
+			url:"",
+			dataType:"json",
+			data:$scope.user,
+			success:function(data){
+				alert("submit success!");
+			}
+		});
+
+	}
+}])
---
+++ register/register.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('myApp',[]);
+app.controller('regController',['$scope',function($scope){
+
+	$scope.user={
+		/*username:"chenchen",
+		password:"111",
+		confirmPassword:"111"*/
+	};
+	$scope.submitForm = function(){
+		alert("success");
+	}
+}])
